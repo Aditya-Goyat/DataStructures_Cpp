d3561f8f6fc977d594bb42d7860346ea4b62c75c
Aditya Goyat <75666820+Aditya-Goyat@users.noreply.github.com>
2023-01-26 15:33:50 +0530

    Find sum of all nodes in a tree

---
+++ Programming Questions/Trees/SumOfNodes.cpp
@@ -0,0 +1,62 @@
+/*
+    Given a generic tree, find and return the sum of all nodes present in the given tree.
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+//Solution
+int sumOfNodes(TreeNode<int>* root) {
+    int sum = root->data;
+    for(int i = 0; i < root->children.size(); i++){
+        sum += sumOfNodes(root->children[i]);
+    }
+
+    return sum;
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    std::cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    std::queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        std::cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            std::cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    std::cout << sumOfNodes(root);
+}
