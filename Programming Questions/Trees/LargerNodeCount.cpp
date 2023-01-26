d45cba1139d8192d85c4a86f11f5eef7201f7733
Aditya Goyat <75666820+Aditya-Goyat@users.noreply.github.com>
2023-01-26 17:56:37 +0530

    Get number of nodes larger than x

---
+++ Programming Questions/Trees/LargerNodeCount.cpp
@@ -0,0 +1,71 @@
+/*
+Given a tree and an integer x, find and return the number of nodes which contains data greater than x.
+*/
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    std::vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+//Solution
+int getLargeNodeCount(TreeNode<int>* root, int x) {
+    int ans = 0;
+
+    if(root == nullptr)
+        return ans;
+    
+    for(int i = 0; i < root->children.size(); i++){
+        ans = getLargeNodeCount(root->children[i], x);
+    }
+    
+    if(root->data > x)
+        ans++;
+
+    return ans;
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    std::cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    std::queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        std::cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            std::cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    int x;
+    std::cin >> x;
+    std::cout << getLargeNodeCount(root, x);
+}
