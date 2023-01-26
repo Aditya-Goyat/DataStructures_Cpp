8947433593ebdc550f4625a3606e2f2eab222d2c
Aditya Goyat <75666820+Aditya-Goyat@users.noreply.github.com>
2023-01-26 20:40:03 +0530

    Replace the value of nodes with their depth

---
+++ Programming Questions/Trees/ReplaceWithDepth.cpp
@@ -0,0 +1,92 @@
+/*
+You are given a generic tree. You have to replace each node with its depth value.
+You just have to update the data of each node, there is no need to return or print anything.
+*/
+
+#include <iostream>
+#include <queue>
+#include <vector>
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    std::vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+//Solution
+void replaceWithDepthValueHelper(TreeNode<int>* root, int value){
+    if(root == nullptr)
+        return;
+    
+    root->data = value;
+
+    for(int i = 0; i < root->children.size(); i++){
+        replaceWithDepthValueHelper(root->children[i], value + 1);
+    }
+}
+
+void replaceWithDepthValue(TreeNode<int>* root) {
+    replaceWithDepthValueHelper(root, 0);
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    std::cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    std::queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        std::cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            std::cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+void printLevelATNewLine(TreeNode<int>* root) {
+    std::queue<TreeNode<int>*> q;
+    q.push(root);
+    q.push(NULL);
+    while (!q.empty()) {
+        TreeNode<int>* first = q.front();
+        q.pop();
+        if (first == NULL) {
+            if (q.empty()) {
+                break;
+            }
+            std::cout << std::endl;
+            q.push(NULL);
+            continue;
+        }
+        std::cout << first->data << " ";
+        for (int i = 0; i < first->children.size(); i++) {
+            q.push(first->children[i]);
+        }
+    }
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+    replaceWithDepthValue(root);
+    printLevelATNewLine(root);
+}
